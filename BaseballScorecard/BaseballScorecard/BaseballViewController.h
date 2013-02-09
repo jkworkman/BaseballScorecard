7cf0d27b6c03987cf0ecf1cdbfac7cde796a9c68
JACOB WORKMAN <jkworkman@SMLBMACMINI4.local>
2013-02-09 13:28:06 -0600

    Balls, Strikes, Outs work. Inning works but trouble displaying it.

--- BaseballScorecard/BaseballScorecard/BaseballViewController.h
+++ BaseballScorecard/BaseballScorecard/BaseballViewController.h
@@ -13,5 +13,8 @@
 - (IBAction)PitchedStrike:(id)sender;
 @property (weak, nonatomic) IBOutlet UILabel *PitchedBallLabel;
 @property (weak, nonatomic) IBOutlet UILabel *PitchedStrikeLabel;
+@property (weak, nonatomic) IBOutlet UILabel *RecordedOutLabel;
+@property (weak, nonatomic) IBOutlet UILabel *SideInningLabel;
+@property (weak, nonatomic) IBOutlet UILabel *NumInningLabel;
 
 @end
--- BaseballScorecard/BaseballScorecard/BaseballViewController.m
+++ BaseballScorecard/BaseballScorecard/BaseballViewController.m
@@ -12,6 +12,10 @@
 
 @property (nonatomic) int balls;
 @property (nonatomic) int strikes;
+@property (nonatomic) int outs;
+@property (nonatomic) NSString *sideInning;
+@property (nonatomic) int numInning;
+@property (nonatomic) BOOL isBottomInning;
 @end
 
 @implementation BaseballViewController
@@ -30,9 +34,57 @@
 
 - (IBAction)PitchedBall:(id)sender {
     _balls += 1;
+    
+    if(_balls == 4)
+    {
+        _balls = 0;
+        _strikes = 0;
+    }
+    _PitchedStrikeLabel.text = IntToString(_strikes);
+    _PitchedBallLabel.text = IntToString(_balls);
+    _RecordedOutLabel.text = IntToString(_outs);
 }
 
 - (IBAction)PitchedStrike:(id)sender {
     _strikes += 1;
+    if(_strikes == 3)
+    {
+        _outs += 1;
+        _strikes = 0;
+        _balls = 0;
+    }
+    if(_outs == 3)
+    {
+        _outs = 0;
+        if(_isBottomInning)
+        {
+            _sideInning = @"Top";
+            _numInning += 1;
+        }
+        else
+        {
+            _sideInning = @"Bottom";
+        }
+        
+        if(_isBottomInning)
+        {
+            _isBottomInning = false;
+        }
+        else
+        {
+            _isBottomInning = true;
+        }
+        
+    }
+    _PitchedStrikeLabel.text = IntToString(_strikes);
+    _PitchedBallLabel.text = IntToString(_balls);
+    _RecordedOutLabel.text = IntToString(_outs);
+    _SideInningLabel.text = _sideInning;
+    _NumInningLabel.text = IntToString(_numInning);
+}
+NSString *IntToString(int p)
+{
+    NSString *greeting = [[NSString alloc] initWithFormat:@"%d", p];
+    return greeting;
 }
 @end
